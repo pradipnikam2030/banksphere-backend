714c6f0fd530fed43f37a9509ac25b27ac0695ad
Pradip Nikam <nikampradip863@gmail.com>
2026-07-25 00:00:14 +0530

    initial project setup

---
+++ src/main/java/com/pradip/banksphere/BanksphereApplication.java
@@ -0,0 +1,13 @@
+package com.pradip.banksphere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BanksphereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BanksphereApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pradip/banksphere/BanksphereApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pradip.banksphere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BanksphereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
